193b3e519dba810baacd45a73f00aed4686bde71
jxstrian <rian@ekr.moe>
2024-10-21 16:35:20 +0900

    fixed permissions

--- bot/groups.py
+++ bot/groups.py
@@ -23,8 +23,7 @@ __all__ = ["music", "admin"]
 
 music = crescent.Group(
     "music",
-    dm_enabled=False,
-    default_member_permissions=hikari.Permissions.MANAGE_GUILD
+    dm_enabled=False
 )
 
 admin = crescent.Group(
