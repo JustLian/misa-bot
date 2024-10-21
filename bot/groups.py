648cad67159ab4d0fbd16a69958cca5303be1d35
jxstrian <rian@ekr.moe>
2024-10-21 16:32:25 +0900

    fixed group permission

--- bot/groups.py
+++ bot/groups.py
@@ -1,5 +1,34 @@
 import crescent
+import hikari
+import toolbox
 
 
-music = crescent.Group("music")
-admin = crescent.Group("admin")
+__all__ = ["music", "admin"]
+
+
+# async def _admin_hook(ctx: crescent.Context) -> crescent.HookResult:
+#     if (
+#         toolbox.calculate_permissions(ctx.member)
+#         .intersection(hikari.Permissions.MANAGE_GUILD)
+#     ) == hikari.Permissions.NONE:
+#         await ctx.respond(embed=hikari.Embed(
+#             title='Недостаточно прав',
+#             description='Эту команду могут использовать только администраторы сервера',
+#             color="#000000"
+#         ), flags=hikari.MessageFlag.EPHEMERAL)
+#         return crescent.HookResult(exit=True)
+
+#     return crescent.HookResult()
+
+
+music = crescent.Group(
+    "music",
+    dm_enabled=False,
+    default_member_permissions=hikari.Permissions.MANAGE_GUILD
+)
+
+admin = crescent.Group(
+    "admin",
+    dm_enabled=False,
+    default_member_permissions=hikari.Permissions.MANAGE_GUILD
+)
